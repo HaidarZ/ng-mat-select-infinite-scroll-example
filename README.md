578b9c654393c5fcde2bd5cfb4d2bc1fc4588f9b
HaidarZ <haidar.zeineddine@gmail.com>
2019-03-29 22:28:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-lreni5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-lreni5)
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+    
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {MatSelectInfiniteScrollModule} from 'ng-mat-select-infinite-scroll';
+import {MatFormFieldModule, MatSelectModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSelectInfiniteScrollModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
